147368c7fa2039396d0a635704ad0fc481fc7b24
WayneCommand <waynecommand@gmail.com>
2024-10-01 11:13:57 +0800

    Update uninotify worker to refactor notify class and add new platforms

--- src/worker.js
+++ src/worker.js
@@ -8,14 +8,14 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-class Notify {
-	constructor(apiToken, platform, title, content) {
-		this.apiToken = apiToken;
-		this.platform = platform;
-		this.title = title ? title : "Server Notify";
-		this.content = content ? content : "this is a default message, plz check your server status.";
-	}
-}
+const ftqqEndpoint = "https://sctapi.ftqq.com/";
+const iyuuEndpoint = "https://iyuu.cn/";
+
+const iyuuPlatform = "iyuu";
+const ftqqPlatform = "ftqq";
+
+const platforms = ["ftqq", "iyuu"];
+
 
 export default {
   async fetch(request, env, ctx) {
@@ -25,8 +25,13 @@ export default {
 	  // router
 	  switch (pathname) {
 		  case "/wechat": {
+			  const options = {
+				  apiKey: env.api_key,
+				  ftqqToken: env.ftqq_token,
+				  iyuuToken: env.iyuu_token
+			  }
 
-			  return wechatPlatform(request, env, ctx)
+			  return wechatPlatform(request, options)
 		  }
 
 
@@ -35,54 +40,57 @@ export default {
   },
 };
 
-const wechatPlatform = async (request, env, ctx) => {
+const wechatPlatform = async (request, options) => {
 
 	if (request.method === 'GET') return Response.json({"error": "method not supported."})
 
 	// 简单版的权限校验
 	// Authorization: Bearer $OPENAI_API_KEY
 	const authorization = request.headers.get("Authorization");
-	if (authorization !== `Bearer ${env.api_key}`) return Response.json({"message": "No permission invoke API"});
+	if (authorization !== `Bearer ${options.apiKey}`) return Response.json({"message": "No permission invoke API"});
 
 	const body = await request.json();
 
-	let resp = Response.json({"message": "notify sent."})
+	// 校验平台
+	if (body.platform) {
+		if (platforms.indexOf(body.platform) === -1) return Response.json({"message": "platform not supported."})
+	}
+
+	let notify = new Notify(body.apiToken, body.platform, body.title, body.content, "default");
+
 
 	// platform
 	switch (body.platform) {
-		case "ftqq": {
-			resp = await ftqq(new Notify(body.apiToken? body.apiToken : env.ftqq_token, "ftqq", body.title, body.content))
-
+		case ftqqPlatform: {
+			notify.platform = ftqqPlatform
+			notify.endpoint = ftqqEndpoint
+			notify.apiToken = options.ftqqToken
 			break
 		}
-		// default
 		default: {
-			resp = await iyuu(new Notify(body.apiToken? body.apiToken: env.iyuu_token, "iyuu", body.title, body.content))
+			notify.platform = iyuuPlatform
+			notify.endpoint = iyuuEndpoint
+			notify.apiToken = options.iyuuToken
 		}
-
 	}
+
+	let resp = await serverChanCompatibleAPI(notify);
 
 	return Response.json(resp)
 }
 
-
 /**
  * @param {Notify} notify
  * @returns {any} token json
  */
-const iyuu = async (notify) => {
-
-	const notify_uri = `https://iyuu.cn/${notify.apiToken}.send`;
-
-	let formData = new URLSearchParams()
-	formData.set("text", notify.title)
-	formData.set("desp", notify.content)
+const serverChanCompatibleAPI = async (notify) => {
+	const notify_uri = notify.endpoint + notify.apiToken + ".send";
 
 	const _init = {
 		// Change method
 		method: "POST",
 		// Change body
-		body: formData,
+		body: new URLSearchParams({text: notify.title, desp: notify.content}),
 		// Change the redirect mode.
 		redirect: "follow",
 		// Change headers, note this method will erase existing headers
@@ -93,45 +101,20 @@ const iyuu = async (notify) => {
 		cf: { apps: false },
 	}
 
-	const tokenRequest = new Request(notify_uri, _init)
+	const response = await fetch(new Request(notify_uri, _init))
 
-	const token = await fetch(tokenRequest)
+	console.log(response)
 
 	// default resp
-	return await token.json()
+	return await response.json()
 }
 
-/**
- * @param {Notify} notify
- * @returns {any} token json
- */
-const ftqq = async (notify) => {
-
-	const notify_uri = `https://sctapi.ftqq.com/${notify.apiToken}.send`;
-
-	let formData = new URLSearchParams()
-	formData.set("text", notify.title)
-	formData.set("desp", notify.content)
-
-	const _init = {
-		// Change method
-		method: "POST",
-		// Change body
-		body: formData,
-		// Change the redirect mode.
-		redirect: "follow",
-		// Change headers, note this method will erase existing headers
-		headers: {
-			"Content-Type": "application/x-www-form-urlencoded"
-		},
-		// Change a Cloudflare feature on the outbound response
-		cf: { apps: false },
+class Notify {
+	constructor(apiToken, platform, title, content, endpoint) {
+		this.apiToken = apiToken;
+		this.platform = platform;
+		this.title = title ? title : "Server Notify";
+		this.content = content ? content : "this is a default message, plz check your server status.";
+		this.endpoint = endpoint;
 	}
-
-	const tokenRequest = new Request(notify_uri, _init)
-
-	const token = await fetch(tokenRequest)
-
-	// default resp
-	return await token.json()
 }
